0b15559879555af1b3777bb94701a7f212d9342a
leemcf4 <leemcf4@hotmail.co.uk>
2024-01-09 16:37:12 +0000

    mobile nav button

--- src/components/elements/buttons/MobileNavButton.tsx
+++ src/components/elements/buttons/MobileNavButton.tsx
@@ -6,31 +6,34 @@ const MobileNavButton = ({ mobileMenuOpen, setMobileMenuOpen, scrolledBg }) => {
 			onClick={() => setMobileMenuOpen((prev) => !prev)}
 			className={`h-full p-[15px] transition-colors duration-300 md-large:hidden ${scrolledBg || mobileMenuOpen ? "bg-black" : "bg-transparent"}`}
 		>
-			<svg
-				className={` transition-transform duration-300 ${mobileMenuOpen ? "translate-x-[2px]" : ""}`}
-				width="19"
-				height="19"
-				viewBox="0 0 19 19"
-				fill="none"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<g className="translate-y-[2px]">
-					<rect
-						className={`transform transition-transform duration-300 will-change-transform ${mobileMenuOpen ? "translate-y-[-4px] rotate-45" : ""}`}
-						width="22.588"
-						height="2.62044"
-						fill="currentColor"
-					/>
-				</g>
-
-				<g className="translate-y-[10px]">
-					<rect
-						className={`transform transition-transform duration-300 will-change-transform ${mobileMenuOpen ? "translate-y-[3px] rotate-[-45deg]" : ""}`}
-						width="22.588"
-						height="2.62044"
-						fill="currentColor"
-					/>
-				</g>
+			<svg overflow="visible" width="24" height="18" viewBox="0 0 24 18" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<rect
+					className={`!origin-[12px_2.38px]
+					${mobileMenuOpen ? "translate-y-[7.25px] rotate-45" : "	translate-y-0 rotate-0"}
+					transition-transform
+					duration-200
+				`}
+					y="0.570007"
+					width="24"
+					height="2.62"
+					fill="currentColor"
+				/>
+				<rect
+					className={`!origin-center transition-transform duration-200 ${mobileMenuOpen ? "opacity-0" : "opacity-100"}`}
+					y="7.69"
+					width="24"
+					height="2.62"
+					fill="currentColor"
+				/>
+				<rect
+					className={`!origin-[12px_16.62px] transition-transform	duration-200
+					${mobileMenuOpen ? "translate-y-[-7.25px] rotate-[-45deg]" : "translate-y-0 rotate-0"}
+				`}
+					y="14.81"
+					width="24"
+					height="2.62"
+					fill="currentColor"
+				/>
 			</svg>
 		</button>
 	);
