2f557d505d613d7034640689e019e57b3f0fe61a
leemcf4 <leemcf4@hotmail.co.uk>
2024-02-05 15:42:12 +0000

    setup sector filter

--- src/components/flexible/workBlocks/AllWorkGrid.tsx
+++ src/components/flexible/workBlocks/AllWorkGrid.tsx
@@ -15,38 +15,61 @@ const AllWorkGrid = (props) => {
 
 	const defaultWorkType = workPosts[0]?.["work-type"][0]?.slug;
 	const defaultService = "All";
+	const defaultSector = "All";
 
 	const [selectedService, setSelectedService] = useState(defaultService) as any;
+	const [selectedSector, setSelectedSector] = useState(defaultSector) as any;
 	const [selectedWorkType, setSelectedWorkType] = useState(defaultWorkType) as any;
 	const [displayedPostsCount, setDisplayedPostsCount] = useState(LOAD_MORE_AMOUNT);
+
+	const [sectorManuallySet, setSectorManuallySet] = useState(false);
 
 	useEffect(() => {
 		const { query } = router;
 		setSelectedWorkType(query.work_type || defaultWorkType);
 		setSelectedService(query.service || defaultService);
-	}, [router.query, defaultWorkType, defaultService, router]);
+		// Remove this after sectors filters are designed and use the commented line below
+		if (!sectorManuallySet) {
+			setSelectedSector(query.sector || defaultSector);
+		}
 
-	const updateUrlParams = (service, workType) => {
+		// setSelectedSector(query.sector || defaultSector);
+	}, [router.query, defaultWorkType, defaultService, defaultSector, router, sectorManuallySet]);
+
+	const updateUrlParams = (service, workType, sector) => {
 		router.push(
 			{
 				pathname: router.pathname,
-				query: { ...router.query, service, work_type: workType },
+				query: { ...router.query, service, work_type: workType, sector },
 			},
 			undefined,
 			{ shallow: true }
 		);
 	};
 
+	const handleSectorSelection = (slug) => {
+		setSelectedSector(slug);
+		updateUrlParams(selectedService, selectedWorkType, slug);
+	};
+
 	const handleServiceSelection = (slug) => {
 		setSelectedService(slug);
-		updateUrlParams(slug, selectedWorkType);
+		updateUrlParams(slug, selectedWorkType, selectedSector);
 	};
 
 	const handleWorkTypeSelection = (slug) => {
 		setSelectedWorkType(slug);
-		setSelectedService(defaultService);
-		updateUrlParams(defaultService, slug);
+		// remove these lines after sectors filters are designed
+		setSelectedSector("All");
+		setSectorManuallySet(false);
+		//
+
+		if (slug !== "All") {
+			setSelectedService(defaultService);
+		}
+		updateUrlParams(selectedService, slug, "All");
 	};
+
 	const handleLoadMore = () => {
 		setDisplayedPostsCount((prevCount) => prevCount + LOAD_MORE_AMOUNT);
 	};
@@ -58,24 +81,66 @@ const AllWorkGrid = (props) => {
 		});
 	});
 
-	const availableServices = workPosts
-		.filter((work) => work["work-type"].some((type) => type.slug === selectedWorkType))
+	const uniqueSectors = new Map([["All", "All"]]);
+	workPosts.forEach((work) => {
+		work.sector.forEach((sector) => {
+			if (!uniqueSectors.has(sector.slug)) {
+				uniqueSectors.set(sector.slug, sector.name);
+			}
+		});
+	});
+
+	const filteredPostsForServices = workPosts.filter((work) => {
+		const workTypeMatch = selectedWorkType === "All" || work["work-type"].some((type) => type.slug === selectedWorkType);
+		const sectorMatch = selectedSector === "All" || work.sector.some((sector) => sector.slug === selectedSector);
+		return workTypeMatch && sectorMatch;
+	});
+
+	const filteredPostsForSectors = workPosts.filter((work) => {
+		const serviceMatch = selectedService === "All" || work.services.some((service) => service.slug === selectedService);
+		const workTypeMatch = selectedWorkType === "All" || work["work-type"].some((type) => type.slug === selectedWorkType);
+		return serviceMatch && workTypeMatch;
+	});
+
+	const availableSectors = filteredPostsForSectors
+		.flatMap((work) => work.sector)
+		.reduce(
+			(acc, sector) => {
+				acc.set(sector.slug, sector.name);
+				return acc;
+			},
+			new Map([["All", "All"]])
+		);
+
+	const availableServices = filteredPostsForServices
 		.flatMap((work) => work.services)
-		.reduce((acc, service) => acc.set(service.name, service.slug), new Map([["All", "All"]]));
+		.reduce(
+			(acc, service) => {
+				const key = service.name;
+				if (!acc.has(key)) {
+					acc.set(key, service.slug);
+				}
+				return acc;
+			},
+			new Map([["All", "All"]])
+		);
 
 	const workTypeNames = Array.from(uniqueWorkTypes.entries());
 	const serviceNames = Array.from(availableServices.entries()) as any;
 	const filteredAndSlicedWorkPosts = workPosts.filter((work) => {
 		const serviceMatch = selectedService === "All" || work.services.some((service) => service.slug === selectedService);
-		const workTypeMatch = work["work-type"].some((type) => type.slug === selectedWorkType);
-		return serviceMatch && workTypeMatch;
+		const workTypeMatch = selectedWorkType === "All" || work["work-type"].some((type) => type.slug === selectedWorkType); // Check for 'all' slug
+		const sectorMatch = selectedSector === "All" || work.sector.some((sector) => sector.slug === selectedSector);
+		return serviceMatch && workTypeMatch && sectorMatch;
 	});
+
+	const sectorOptions = Array.from(availableSectors, ([slug, name]) => ({ slug, name }));
 
 	const filteredWorkPosts = filteredAndSlicedWorkPosts.slice(0, displayedPostsCount);
 	const allPostsDisplayed = displayedPostsCount >= filteredAndSlicedWorkPosts.length;
 
 	return (
-		<div>
+		<div id="all-work-grid" className="scroll-mt-20">
 			<div className="mb-8 flex flex-col items-center gap-y-5 md:mb-20 lg:flex-row lg:justify-between">
 				<ul className="t-15  flex gap-4 font-bold lg:gap-2">
 					{workTypeNames.map(([name, { slug, hoverName }], i) => (
@@ -86,8 +151,8 @@ const AllWorkGrid = (props) => {
 							onKeyDown={() => handleWorkTypeSelection(slug)}
 						>
 							<HoverButton
-								buttonClass={` whitespace-nowrap ${selectedWorkType === slug ? "!bg-cobalt !text-electric" : ""}`}
-								externalFontTrigger={selectedWorkType === slug}
+								buttonClass={` whitespace-nowrap ${selectedWorkType === slug || selectedWorkType === "All" ? "!bg-cobalt !text-electric" : ""}`}
+								externalFontTrigger={selectedWorkType === slug || selectedWorkType === "All"}
 								aria-label={name}
 								type="button"
 								button={{
@@ -138,24 +203,61 @@ const AllWorkGrid = (props) => {
 							</button>
 						))}
 					</div>
+					{/* <div className="t-15-large  hide-scrollbars relative flex min-h-[40px] w-full gap-4 overflow-x-auto  px-2 font-bold sm:justify-center sm:px-0 lg:w-auto xl:gap-8">
+						{sectorOptions.map(({ slug, name }, i) => (
+							<button
+								aria-label={name}
+								type="button"
+								key={`sector-${i}`}
+								className=" block cursor-pointer transition-colors duration-200 hover:text-cobalt "
+								onClick={() => handleSectorSelection(slug)}
+							>
+								<div className="relative whitespace-nowrap">
+									<span dangerouslySetInnerHTML={{ __html: name }} />
+
+									<div className="absolute bottom-[-6px] left-0 flex w-full justify-center">
+										{selectedSector === slug && (
+											<motion.span
+												layoutId="sectorUnderline"
+												transition={{
+													layout: {
+														type: "spring",
+														stiffness: 300,
+														damping: 28,
+													},
+												}}
+												className=" h-[6px] justify-center will-change-transform"
+											>
+												<motion.span layout="position" className="block h-[6px] w-[6px] bg-cobalt" />
+											</motion.span>
+										)}
+									</div>
+								</div>
+							</button>
+						))}
+					</div> */}
 				</div>
 			</div>
 			<div className="grid gap-3 sm:grid-cols-2 md:gap-5 lg:grid-cols-3 xl:gap-x-8 xl:gap-y-20">
-				{filteredWorkPosts.map((work, i) => (
-					<motion.div
-						key={`work-${i}-${selectedWorkType}`}
-						initial={{ opacity: 0, scale: 0.8 }}
-						animate={{
-							opacity: 1,
-							scale: 1,
-						}}
-						transition={{
-							delay: i * 0.05,
-						}}
-					>
-						<WorkGridItem work={work} variant="3col" />
-					</motion.div>
-				))}{" "}
+				{filteredWorkPosts?.length > 0 ? (
+					filteredWorkPosts.map((work, i) => (
+						<motion.div
+							key={`work-${i}-${selectedWorkType}`}
+							initial={{ opacity: 0, scale: 0.8 }}
+							animate={{
+								opacity: 1,
+								scale: 1,
+							}}
+							transition={{
+								delay: i * 0.05,
+							}}
+						>
+							<WorkGridItem work={work} variant="3col" />
+						</motion.div>
+					))
+				) : (
+					<div className="t-18 pl-8">No work found</div>
+				)}
 			</div>
 			{!allPostsDisplayed && (
 				<div className="mt-12 flex justify-center md:mt-32">
