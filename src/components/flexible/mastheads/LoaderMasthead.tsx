a5d7c31f484951801d723a48596aec0e1ab7cef1
leemcf4 <leemcf4@hotmail.co.uk>
2024-02-28 13:03:12 +0000

    remove heading

--- src/components/flexible/mastheads/LoaderMasthead.tsx
+++ src/components/flexible/mastheads/LoaderMasthead.tsx
@@ -1,6 +1,5 @@
 import Asset from "~/components/elements/Asset";
 import { motion, useInView } from "framer-motion";
-import FontSwitcher from "~/components/elements/animations/helpers/FontSwitcher";
 import { useMeasure, useWindowSize } from "react-use";
 import { useContext, useEffect, useRef, useState } from "react";
 import { VideoLoadedContext } from "~/utils/context";
@@ -51,16 +50,16 @@ const LoaderMasthead = (props) => {
 	const assetYEndValue = videoLoaded ? 0 : -assetInitialY;
 
 	return (
-		<div className="relative overflow-hidden bg-black pb-20 text-white md:pb-32   lg:min-h-screen tiny-laptop:pb-20 ">
+		<div className="relative min-h-[280px] overflow-hidden  bg-black text-white lg:min-h-screen ">
 			<div ref={containerRef} className="container absolute inset-0 flex items-center justify-center lg:h-screen">
 				<div ref={logoRef} className="mx-auto w-full max-w-[60vw] md:max-w-[500px]">
 					<LogoLoader videoLoaded={videoLoaded} />
 				</div>
 			</div>
-			<div className="mx-auto w-full max-w-[3000px]  ">
-				<div className="">
-					<div ref={assetRef} className="">
-						<div ref={ref}>
+			<div className="absolute inset-0 mx-auto h-full w-full max-w-[3000px] md:absolute ">
+				<div className="h-full w-full">
+					<div ref={assetRef} className="h-full w-full">
+						<div ref={ref} className="h-full w-full">
 							<motion.div
 								initial={{
 									clipPath: "inset(0% 50% 0% 50%)",
@@ -81,7 +80,7 @@ const LoaderMasthead = (props) => {
 										onComplete: () => setAnimationComplete(true),
 									},
 								}}
-								className="bg-stone/0.01 mx-auto aspect-[1920/1080] w-full will-change-transform laptop:max-h-[90vh]  "
+								className="bg-stone/0.01 mx-auto  h-full w-full  will-change-transform "
 							>
 								<motion.div
 									initial={{
@@ -105,53 +104,9 @@ const LoaderMasthead = (props) => {
 							</motion.div>
 						</div>
 					</div>
-
-					<div className="px-8">
-						<h1 className="t-144-xxlarge mx-auto mt-20 max-w-[1376px] text-center font-black uppercase md:mt-32 tiny-laptop:mt-20">
-							<span className="">
-								<TextContainer startAnimation={videoLoaded} custom={4}>
-									<FontSwitcher startDelay={3800} text="H<pst-grid-pst>e</>llo " />
-								</TextContainer>{" "}
-								<TextContainer startAnimation={videoLoaded} custom={3}>
-									welcome
-								</TextContainer>{" "}
-								<br />
-								<span className="whitespace-nowrap">
-									<TextContainer startAnimation={videoLoaded} custom={3}>
-										to
-									</TextContainer>{" "}
-									<TextContainer startAnimation={videoLoaded} custom={2}>
-										{" "}
-										<FontSwitcher startDelay={4000} text="newg<pst-grid-pst>e</>n" />
-									</TextContainer>{" "}
-								</span>
-							</span>
-						</h1>
-					</div>
 				</div>
 			</div>
 		</div>
 	);
 };
 export default LoaderMasthead;
-
-const TextContainer = ({ children, custom = 0, startAnimation }) => {
-	return (
-		<div className="inline-flex ">
-			<motion.span
-				initial={{
-					y: "0.2em",
-					opacity: 0,
-				}}
-				animate={{
-					y: startAnimation ? 0 : "0.2em",
-					opacity: startAnimation ? 1 : 0,
-					transition: { type: "spring", stiffness: 1200, damping: 15, mass: 0.2, delay: 3.5 + custom * 0.1 },
-				}}
-				style={{ display: "inline-flex", willChange: "transform" }}
-			>
-				{children}
-			</motion.span>
-		</div>
-	);
-};
