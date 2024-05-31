e55575d317ce2897a99c6983c266f90394b719ad
leemcf4 <leemcf4@hotmail.co.uk>
2024-05-31 10:33:40 +0100

    remove prop

--- src/components/templates/Layout.tsx
+++ src/components/templates/Layout.tsx
@@ -24,7 +24,7 @@ function Layout({ data, children, hideCTA = false }: LayoutProps) {
 	return (
 		<PasswordProtect password={page?.post_password}>
 			<VideoLoadedContext.Provider value={{ videoLoaded, setVideoLoaded }}>
-				<Seo page={data?.page} globalScripts={options?.global_scripts} />
+				<Seo page={data?.page} />
 				<Header {...options} pageOptions={page?.page_options} pagePostType={page?.post_type} />
 				<main>{children}</main>
 				{!(page?.page_options?.remove_cta || hideCTA) && <CTA pageOptions={page?.page_options} />}
