167b9d0d393778a19442f0abeea85ebea3c61a35
garethtogether <gareth@together.agency>
2024-06-07 08:38:39 +0100

    job listings

--- src/components/flexible/specialBlocks/Jobs.tsx
+++ src/components/flexible/specialBlocks/Jobs.tsx
@@ -1,89 +1,103 @@
-import { useState } from "react";
+import Link from "next/link";
+import { useEffect, useState, ChangeEvent } from "react";
 import FontSwitcher from "~/components/elements/animations/helpers/FontSwitcher";
 
-const JobData = [
-	{
-		title: "cre<pst-grid>a</>tive director",
-		location: "London, UK",
-		type: "Full-time",
-	},
-	{
-		title: "m<pst-grid>a</>rketing specialist",
-		location: "Manchester, UK",
-		type: "Full-time",
-	},
-	{
-		title: "Infl<pst-grid>u</>encer Talent M<pst-grid>a</>nager",
-		location: "Manchester, UK",
-		type: "Full-time",
-	},
-	{
-		title: "content manag<pst-pil>e</>r",
-		location: "New York, UK",
-		type: "Full-time",
-	},
-	{
-		title: "public r<pst-bec>e</>lations",
-		location: "Manchester, UK / Remote",
-		type: "Full-time",
-	},
-];
+interface Job {
+	url: string;
+	title: string;
+	country: string;
+	employment_type: string;
+}
 
 const Jobs = (props) => {
+	const [jobListings, setJobListings] = useState<Job[]>([]);
+	const [uniqueCountries, setUniqueCountries] = useState<string[]>([]);
+	const [selectedCountry, setSelectedCountry] = useState<string>("");
+	const [filteredJobs, setFilteredJobs] = useState<Job[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch("https://apply.workable.com/api/v1/widget/accounts/468490");
+
+				if (!response.ok) {
+					throw new Error("Network response was not ok");
+				}
+
+				const jsonData = await response.json();
+				setJobListings(jsonData.jobs);
+
+				const uniqueCountriesSet = new Set<string>(jsonData.jobs.map((job: Job) => job.country));
+				const uniqueCountriesArray: string[] = Array.from(uniqueCountriesSet);
+				setUniqueCountries(uniqueCountriesArray);
+				setFilteredJobs(jsonData.jobs);
+			} catch (error) {
+				console.error("Error:", error);
+			}
+		};
+
+		fetchData();
+	}, []);
+
+	useEffect(() => {
+		if (selectedCountry === "") {
+			setFilteredJobs(jobListings);
+		} else {
+			const filtered = jobListings.filter((job) => job.country === selectedCountry);
+			setFilteredJobs(filtered);
+		}
+	}, [selectedCountry, jobListings]);
+
+	const handleCountryChange = (event: ChangeEvent<HTMLSelectElement>) => {
+		setSelectedCountry(event.target.value);
+	};
+
 	return (
 		<div>
 			<div className="container flex justify-center gap-5 md:justify-start md:gap-16">
-				<Select
+				{/* <Select
 					options={[
 						{
 							label: "Agency",
 							value: "all",
 						},
 					]}
-				/>
-				<Select
-					options={[
-						{
-							label: "Location",
-							value: "all",
-						},
-					]}
-				/>
+				/> */}
+				<Select options={uniqueCountries} onChange={handleCountryChange} />
 			</div>
 			<div className="mt-10 divide-y divide-ketchup/10 px-5 md:mt-20 md:divide-y-0 md:px-0">
-				{JobData.map((item, i) => (
-					<JobItem key={`job${i}`} {...item} />
-				))}
+				{filteredJobs && filteredJobs.map((item, i) => <JobItem key={`job${i}`} {...item} />)}
 			</div>
 		</div>
 	);
 };
 export default Jobs;
 
-const JobItem = ({ title, location, type }) => {
+const JobItem = ({ title, country, employment_type, url }) => {
 	const [isHovered, setIsHovered] = useState(false);
 
 	return (
-		<div
+		<Link
+			href={url}
 			onMouseEnter={() => setIsHovered(true)}
 			onMouseLeave={() => setIsHovered(false)}
-			className="py-6 !text-black transition-colors  duration-200 md:py-12 md:hover:bg-ketchup hover:md:!text-candy"
+			className="block py-6 !text-black  transition-colors duration-200 md:py-12 md:hover:bg-ketchup hover:md:!text-candy"
 		>
 			<div className="container flex w-full cursor-pointer flex-col items-center justify-center gap-4 text-center md:flex-row md:items-center md:justify-between md:text-left ">
 				<div className="t-32 uupercase max-w-[500px] flex-1 font-heading font-black  uppercase">
 					<FontSwitcher hover isHovered={isHovered} text={title} />
 				</div>
 				<div className="flex w-full flex-1 justify-center gap-4 md:max-w-[240px]">
-					<div className="t-20 font-medium">{location}</div>
-					<div className="t-20 font-medium md:hidden md:max-w-[235px] md:flex-[0.4]">{type}</div>
+					<div className="t-20 font-medium">{country}</div>
+					<div className="t-20 font-medium md:hidden md:max-w-[235px] md:flex-[0.4]">{employment_type}</div>
 				</div>
-				<div className="t-20 hidden font-medium md:block md:max-w-[235px] md:flex-[0.4]">{type}</div>
+				<div className="t-20 hidden font-medium md:block md:max-w-[235px] md:flex-[0.4]">{employment_type}</div>
 			</div>
-		</div>
+		</Link>
 	);
 };
 
-const Select = ({ options }) => {
+const Select = ({ options, onChange }) => {
 	return (
 		<div className="t-24 relative -ml-2 font-medium">
 			<select
@@ -91,10 +105,12 @@ const Select = ({ options }) => {
 					backgroundImage: "unset",
 				}}
 				className="appearance-none border-none bg-transparent focus:ring-0   "
+				onChange={onChange}
 			>
+				<option value="">All</option>
 				{options?.map((option, i) => (
-					<option key={`option${i}`} value={option?.value}>
-						{option?.label}
+					<option key={`option${i}`} value={option}>
+						{option}
 					</option>
 				))}
 			</select>
