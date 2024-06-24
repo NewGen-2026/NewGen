21630292fc48e899560f5032eb2c31ac963fd3cb
Dom <dom@together.agency>
2024-06-24 14:35:38 +0100

    GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,18 @@ export default function MyApp({ Component, pageProps }) {
 						<Script key={`header-${index}`} id={attributes.id || `header-script-${index}`} {...attributes} dangerouslySetInnerHTML={{ __html: content }} />
 					);
 				})}
+			<Script
+				async
+				strategy="lazyOnload"
+				id="google-tag-manager"
+				dangerouslySetInnerHTML={{
+					__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+					new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+					j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+					'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+					})(window,document,'script','dataLayer','GTM-596WKDM5');`,
+				}}
+			/>
 			<Component {...pageProps} />
 		</>
 	);
