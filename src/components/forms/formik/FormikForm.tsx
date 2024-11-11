9b006987199aea6efe951c42e5c93c0f7dec8474
garethtogether <gareth@together.agency>
2024-11-11 13:45:14 +0000

    test Recaptcha

--- src/components/forms/formik/FormikForm.tsx
+++ src/components/forms/formik/FormikForm.tsx
@@ -5,6 +5,7 @@ import { Formik, Form, useFormikContext, useField } from "formik";
 import * as Yup from "yup";
 import Script from "next/script";
 import Head from "next/head";
+import ReCAPTCHA from "react-google-recaptcha";
 
 declare global {
 	interface Window {
@@ -18,6 +19,8 @@ declare global {
 }
 
 const FormikForm = ({ fields, onSubmit, formLayout }) => {
+	const recaptchaRef = React.createRef();
+
 	const validationSchema = Yup.object().shape(
 		fields.reduce((schema, field) => {
 			return { ...schema, [field.name]: field.validation };
@@ -28,21 +31,28 @@ const FormikForm = ({ fields, onSubmit, formLayout }) => {
 		return { ...values, [field.name]: field.initialValue !== undefined ? field.initialValue : "" };
 	}, {});
 	const [isRecaptchaLoaded, setIsRecaptchaLoaded] = useState(false);
+
+	useEffect(() => {
+		console.log("recaptchaRef", recaptchaRef);
+	}, [recaptchaRef]);
+
+	console.log(recaptchaRef);
 	console.log("isRecaptchaLoaded", isRecaptchaLoaded);
 	return (
 		<>
-			<Script
+			{/* <Script
 				src="https://www.google.com/recaptcha/api.js?render=6Lf-XnsqAAAAABGFjzGFsbkrcQkPk-LJXtj_E6nU"
 				onLoad={() => {
 					console.log("SCRIPT LOADED");
 					setIsRecaptchaLoaded(true);
 				}}
-			/>
+			/> */}
 
 			<Formik initialValues={initialValues} validationSchema={validationSchema} onSubmit={onSubmit}>
 				{(formik) => {
 					useEffect(() => {
 						console.log("USEFFECT", isRecaptchaLoaded, window.grecaptcha, window);
+
 						if (isRecaptchaLoaded && window.grecaptcha) {
 							window.grecaptcha.enterprise.ready(() => {
 								window.grecaptcha.enterprise.execute("6Lf-XnsqAAAAABGFjzGFsbkrcQkPk-LJXtj_E6nU", { action: "LOGIN" }).then((token) => {
@@ -66,6 +76,9 @@ const FormikForm = ({ fields, onSubmit, formLayout }) => {
 					return <Form className="newgen-form w-full">{formLayout}</Form>;
 				}}
 			</Formik>
+			<div className="mt-10">
+				<ReCAPTCHA sitekey="6Lf-XnsqAAAAABGFjzGFsbkrcQkPk-LJXtj_E6nU" ref={recaptchaRef} />
+			</div>
 		</>
 	);
 };
